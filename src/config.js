61bc92b70f58d79156b3c1dc5fa3a247f9a4744d
keshav <office.rkmart@gmail.com>
2021-12-30 14:13:17 +0530

    changed backend url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-// exports.url = 'https://help-result-api.herokuapp.com/api';
-exports.url = 'http://localhost:4000/api';
+exports.url = 'https://help-result-api.herokuapp.com/api';
+// exports.url = 'http://localhost:4000/api';
 exports.roles = [
     'Delete',
     'Add',
